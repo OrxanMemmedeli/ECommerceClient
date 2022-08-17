37511ca456c7af38ab49a08b910f217a9ec43796
Orkhan Mammadali <okus.memmedeli@gmail.com>
2022-08-17 14:06:55 +0400

    lesson 10 (Admin /UI Modul and Components)

---
+++ src/app/admin/components/components.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PoroductsModule } from './poroducts/poroducts.module';
+import { OrdersModule } from './orders/orders.module';
+import { CustomersModule } from './customers/customers.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PoroductsModule,
+    OrdersModule,
+    CustomersModule,
+    DashboardModule
+  ],
+  exports: [
+    ComponentsModule
+  ]
+})
+export class ComponentsModule { }
